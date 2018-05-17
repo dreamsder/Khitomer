149dbc00402bc3d029c1dd1bee93fc425ae3f9cc
dreamsder <cristianmontano@gmail.com>
2018-05-17 17:18:22 -0300

    first index

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Khitomer - Sistema de facuracion</title>
+ 	<!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" >
+</head>
+<body>
+
+
+
+</body>
+</html>
